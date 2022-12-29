cb011657f5d0f570ac7f08a404b73c17a97a7ac7
habitual69 <109840055+habitual69@users.noreply.github.com>
2022-12-30 02:20:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 import pyqrcode
-import png
-from pyqrcode import QRCode
 import time
 import os
 
 
 def menu():
-    name=input("Enter Your Name: ")
-    phoneno=input("Enter Your Phone No.")
-    url=input("Enter Your website ulr (www.example.com)")
-    genqr(name,phoneno,url)
-    
+  name = input("Enter Your Name: ")
+  phoneno = input("Enter Your Phone No.")
+  url = input("Enter Your website ulr (www.example.com)")
+  genqr(name, phoneno, url)
 
-def genqr(name,phoneno,url):
+
+def genqr(name, phoneno, url):
+  if (os.path.exists(f"out/{name}")):
+    os.chdir(f"out/{name}")
+  else:
     os.makedirs(os.path.join("out", f"{name}"))
-    os.chdir(f"out\{name}")
-    data=name+"\n"+phoneno+"\n"+url
+    os.chdir(f"out/{name}")
+    data = name + "\n" + phoneno + "\n" + url
     qrout = pyqrcode.create(data)
     print("Getting Your QRCode Ready")
     print(".")
@@ -27,8 +28,9 @@ def genqr(name,phoneno,url):
     print("....")
     time.sleep(0.5)
     print(".....")
-    qrout.svg(f"{name}.svg", scale = 8)
-    qrout=qrout.png(f"{name}.png", scale = 6)
-    print("Here is your QRCode "+os.listdir())
+    qrout.svg(f"{name}.svg", scale=8)
+    qrout = qrout.png(f"{name}.png", scale=6)
+    print("Here is your QRCode ", os.listdir())
+
 
 menu()
